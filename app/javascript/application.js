336099621157fbd95e23f9936f5c3f24e34a830f
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2024-12-28 16:39:00 +0200

    new repository

---
+++ app/javascript/application.js
@@ -0,0 +1,5 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo";
+import "./controllers";
+//import * as Turbo from "@hotwired/turbo";
+//import { Application } from "@hotwired/stimulus";
